afdf377a6d8da6050cf529734e4513536c9c11be
oszka496 <oszka496@gmail.com>
2020-01-26 11:44:45 +0000

    Remove old api file

--- client/src/currencies/currency.epic.js
+++ client/src/currencies/currency.epic.js
@@ -1,17 +1,18 @@
 import { ofType } from 'redux-observable';
 import { from, of } from 'rxjs';
 import { switchMap, map, catchError } from 'rxjs/operators';
-import api from '../api';
+import * as requests from 'api-new/requests';
+import { currency } from 'api-new/urls';
 import { raiseError } from '../core/message.actions';
 import { currencySlice } from './currency.reducer';
 
 const { actions } = currencySlice;
 
-const CURRENCY_API = api.currency.list();
+const fetchCurrencyApi = requests.get(currency.list());
 
 export const currencyEpic = (actions$) => actions$.pipe(
   ofType(actions.fetchCurrencies.toString()),
-  switchMap(() => from(api.requests.get(CURRENCY_API))),
+  switchMap(() => from(fetchCurrencyApi)),
   map(actions.currenciesFetched),
   catchError(() => of(raiseError('Failed to fetch data'))),
 );
