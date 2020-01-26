833b93aa01665d6b14a08ef901a68057b0afb548
oszka496 <oszka496@gmail.com>
2020-01-26 16:46:19 +0000

    Fix login screen crashing

--- client/src/currencies/currency.epic.js
+++ client/src/currencies/currency.epic.js
@@ -1,18 +1,18 @@
-import { ofType } from 'redux-observable';
-import { from, of } from 'rxjs';
-import { switchMap, map, catchError } from 'rxjs/operators';
 import * as requests from 'api/requests';
 import { currency } from 'api/urls';
+import { ofType } from 'redux-observable';
+import { from, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import { raiseError } from '../core/message.actions';
 import { currencySlice } from './currency.reducer';
 
 const { actions } = currencySlice;
 
-const fetchCurrencyApi = requests.get(currency.list());
+const fetchCurrencyApi = () => requests.get(currency.list());
 
 export const currencyEpic = (actions$) => actions$.pipe(
   ofType(actions.fetchCurrencies.toString()),
-  switchMap(() => from(fetchCurrencyApi)),
+  switchMap(() => from(fetchCurrencyApi())),
   map(actions.currenciesFetched),
   catchError(() => of(raiseError('Failed to fetch data'))),
 );
