d6d59cdfc8fb5c817cdcecf3f75c6e684c0184fe
oszka496 <oszka496@gmail.com>
2020-05-16 17:52:07 +0100

    Server: upgrade deps

--- server/server/settings.py
+++ server/server/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost']
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:8000',
-    'localhost:3000',
+    'http://localhost:8000',
+    'http://localhost:3000',
 )
 
 # Application definition
