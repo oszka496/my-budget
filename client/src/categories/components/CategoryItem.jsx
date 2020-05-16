8497a135e70e6bb61a443cc361e786602ef87ef4
oszka496 <oszka496@gmail.com>
2020-05-16 19:56:23 +0100

    Routes for filtered transactions

--- client/src/categories/components/CategoryItem.jsx
+++ client/src/categories/components/CategoryItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import { ListItem, ListItemIcon, ListItemSecondaryAction, ListItemText } from '@material-ui/core';
 import FolderIcon from '@material-ui/icons/FolderOutlined';
 import AllIcon from '@material-ui/icons/AllInclusive';
@@ -7,16 +7,19 @@ import { coreRoutes } from 'core/core.routes';
 import { AdapterLink } from '../../components/AdapterLink';
 import { ItemActions } from './CategoryItemActions';
 
-export const AllTransactionsItem = () => (
-  <ListItem dense button component={AdapterLink} to={coreRoutes.transactions.href}>
+export const AllTransactionsItem = ({ selected }) => (
+  <ListItem dense button component={AdapterLink} to={coreRoutes.transactions.href} selected={selected}>
     <ListItemIcon>
       <AllIcon color="primary" />
     </ListItemIcon>
     <ListItemText primary="All transactions" />
   </ListItem>
 );
+AllTransactionsItem.propTypes = {
+  selected: PropTypes.bool,
+};
 
-export const CategoryItem = ({ name }) => {
+export const CategoryItem = ({ id, name, selected }) => {
   const [actionsVisible, setVisibility] = useState(false);
   const showActions = () => setVisibility(true);
   const hideActions = () => setVisibility(false);
@@ -30,7 +33,13 @@ export const CategoryItem = ({ name }) => {
 
   return (
     <div {...eventHandlers}>
-      <ListItem dense button component="a" href={`${coreRoutes.transactions.href}#${name}`}>
+      <ListItem
+        dense
+        button
+        component={AdapterLink}
+        to={`${coreRoutes.transactions.href}?category=${id}`}
+        selected={selected}
+      >
         <ListItemIcon>
           <FolderIcon />
         </ListItemIcon>
@@ -43,5 +52,7 @@ export const CategoryItem = ({ name }) => {
   );
 };
 CategoryItem.propTypes = {
-  name: string.isRequired,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  selected: PropTypes.bool,
 };
--- client/src/categories/components/CategoryList.jsx
+++ client/src/categories/components/CategoryList.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { List, ListSubheader } from '@material-ui/core';
 import { selectCategoriesAll } from 'categories/category.selectors';
+import { useURLSearchParams } from 'utils/router.utils';
 import { CategoryItem, AllTransactionsItem } from './CategoryItem';
 
 export const CategoryList = () => {
   const categories = useSelector(selectCategoriesAll);
+  const query = useURLSearchParams();
+  const categoryId = query.get('category');
 
   return (
     <List
@@ -18,8 +21,8 @@ export const CategoryList = () => {
         </ListSubheader>
       )}
     >
-      <AllTransactionsItem />
-      { categories.map(({ id, name }) => <CategoryItem name={name} key={id} />)}
+      <AllTransactionsItem selected={!categoryId} />
+      { categories.map(({ id, name }) => <CategoryItem id={id} name={name} key={id} selected={id === categoryId} />)}
     </List>
   );
 };
